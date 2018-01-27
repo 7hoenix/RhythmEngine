8d48f7dbbf0191f2d5848c4bb3f56de57bff3ae7
7hoenix <jpholzmann@gmail.com>
2018-01-27 14:07:25 -0600

    Add user input support and change Result to an actual type

--- Main.elm
+++ Main.elm
@@ -18,9 +18,14 @@ main =
 -- MODEL
 
 
+type Result
+    = Succeed
+    | Fail
+
+
 type Status
     = InProgress
-    | Finished String
+    | Finished Result
 
 
 type alias Model =
@@ -44,18 +49,27 @@ init =
 
 type Msg
     = UserInput
+    | Reset
     | Tick Time
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        UserInput ->
+        Reset ->
             ( { model | countdown = 3, status = InProgress }, Cmd.none )
+
+        UserInput ->
+            case model.status of
+                InProgress ->
+                    ( { model | status = Finished Succeed }, Cmd.none )
+
+                Finished result ->
+                    ( model, Cmd.none )
 
         Tick _ ->
             if model.countdown < 0 then
-                ( { model | status = Finished "failed" }, Cmd.none )
+                ( { model | status = Finished Fail }, Cmd.none )
             else
                 ( { model | countdown = model.countdown - 1 }, Cmd.none )
 
@@ -82,7 +96,9 @@ view : Model -> Html Msg
 view model =
     div []
         [ div [] [ displayCountDown model.countdown ]
-        , div [ onClick UserInput ] [ text "reset" ]
+        , div [ onClick UserInput ] [ text "click me before the timer runs out" ]
+        , div [] [ text (displayResult model.status) ]
+        , div [ onClick Reset ] [ text "Reset" ]
         ]
 
 
@@ -91,3 +107,12 @@ displayCountDown countdown =
         text (toString 0)
     else
         text (toString countdown)
+
+
+displayResult status =
+    case status of
+        InProgress ->
+            "currently playing"
+
+        Finished result ->
+            toString result
